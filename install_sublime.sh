ab5cb2b4aa03641080ff440cd233b7d325ff647c
Sraleik <nicolas.rotier@gmail.com>
2016-01-30 22:01:49 +0100

    installation complete de sumblie avec package controle et mes plugins

--- install_sublime.sh
+++ install_sublime.sh
@@ -5,3 +5,14 @@
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 
 sudo apt-get update 
 sudo apt-get install -y sublime-text
+
+cd ~/.config
+mkdir sublime-text-3
+cd sublime-text-3
+
+git init
+git remote add origin https://gitlab.com/Sraleik/sublime3Config.git
+
+git pull
+git checkout master
+

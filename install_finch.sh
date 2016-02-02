cad45332a46d9a3df540327ab03392e96cda3917
Sraleik <nicolas.rotier@gmail.com>
2016-02-02 14:50:06 +0100

    modification du xml pour les notifs

--- install_finch.sh
+++ install_finch.sh
@@ -2,7 +2,7 @@
 
 ## Fish
 
-sudo apt-get install finch
+sudo apt-get install finch xmlstarlet
 
 cd
 mkdir .purple
@@ -13,3 +13,7 @@ git remote add origin https://gitlab.com/Sraleik/finchConfig.git
 
 git pull
 git checkout master
+
+user=`whoami`
+
+xmlstarlet edit -L -u "/pref[@version='1']/pref[@name='finch']/pref[@name='sound']/pref[@name='profiles']/pref[@name='default']/pref[@name='command']/@value" -v "/home/$user/.purple/notification.sh" prefs.xml

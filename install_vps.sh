26d2fd71b4d86e263aec4999d255b9754b535b45
Sraleik <nicolas.rotier@gmail.com>
2016-12-06 15:26:39 +0100

    first try for install_vps

--- install_vps.sh
+++ install_vps.sh
@@ -7,16 +7,42 @@ read user
 
 echo ""
 
-if [ ${upgrade} = 'install' ]
+if [ ${upgrade} = 'y' ]
 then
   sudo apt-get upgrade -y
 fi
 
-## swapiness
-./config_swapiness.sh
+###### swapiness
+
+echo "swapiness?(y/n)"
+read swapiness
+
+echo ""
+
+if [ ${upgrade} = 'y' ]
+then
+  ./config_swapiness.sh
+fi
 
 ## git
-./config_git.sh
+echo "Config git?(y/n)"
+read setgit
+
+echo ""
+
+if [ ${setgit} = 'y' ]
+then
+  ./config_git.sh
+fi
 
 ## Fish
-./install_fish.sh
+
+echo "Install fish?(y/n)"
+read install_fish
+
+echo ""
+
+if [ ${install_fish} = 'y' ]
+then
+  ./install_fish.sh
+fi

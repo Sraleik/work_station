a4f9483c900bf32dddf94f63be58e3929ba69d6c
Sraleik <nicolas.rotier@gmail.com>
2017-02-20 15:38:59 +0100

    install comf de nvim

--- install_nvim.sh
+++ install_nvim.sh
@@ -7,3 +7,19 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 
 sudo apt-get install neovim
+
+###### nvim config
+
+echo "-------------"
+echo "Config nvim?(y/n)"
+read setnvim
+
+echo ""
+
+if [ "$setnvim" == "y" ]
+then
+  cd ~/.config
+  git clone https://gitlab.com/Sraleik/nvimConfig.git nvim
+  cd nvim
+  git checkout v2
+fi

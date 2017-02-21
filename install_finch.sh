a3fabe5bf4b381108a57fdf85e4bc79509be46d7
Sraleik <nicolas.rotier@gmail.com>
2017-02-21 15:21:54 +0100

    few fix for some kinks

--- install_finch.sh
+++ install_finch.sh
@@ -27,8 +27,8 @@ then
   read finch_gmail_password
 
   echo ""
-  sed -i 's/%EMAIL%/"$finch_gmail"/g' accounts.xml
-  sed -i 's/%PASSWORD%/"$finch_gmail_password"/g' accounts.xml
+  sed -i 's/%EMAIL%/'"$finch_gmail"'/g' accounts.xml
+  sed -i 's/%PASSWORD%/'"$finch_gmail_password"'/g' accounts.xml
   cd -
 fi
 
@@ -41,8 +41,7 @@ echo ""
 if [ "$set_notif" == "y" ]
 then
   cd ~/.purple
-  sed -i 's/%PATH_TO_NOTIF_SCRIPT%/"$finch_gmail"/g' prefs.xml
-  xmlstarlet edit -L -u "/pref[@version='1']/pref[@name='finch']/pref[@name='sound']/pref[@name='profiles']/pref[@name='default']/pref[@name='command']/@value" -v "`pwd`/notification.sh" prefs.xml
+  sed -i 's/%USER%/'`whoami`'/g' prefs.xml
   cd -
 fi
 

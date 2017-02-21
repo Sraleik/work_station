1082dfb174fc064f2e7e29a546d7770635ef415a
Sraleik <nicolas.rotier@gmail.com>
2017-02-21 10:53:43 +0100

    modif of finch

--- install_finch.sh
+++ install_finch.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-## Fish
+## Finch
 
 sudo apt-get install finch xmlstarlet
 
-cd
-mkdir .purple
-cd .purple
+echo "-------------"
+echo "Config finch?(y/n)"
+read config_finch 
 
-git init
-git remote add origin https://gitlab.com/Sraleik/finchConfig.git
+echo ""
 
-git pull
-git checkout master
+if [ "$config_finch" == "y" ]
+then
+  cd
+  git clone https://gitlab.com/Sraleik/finchConfig.git .purple
+  cd -
+fi
 
-user=`whoami`
+echo "-------------"
+echo "Set notification?(y/n)"
+read set_notif
 
-xmlstarlet edit -L -u "/pref[@version='1']/pref[@name='finch']/pref[@name='sound']/pref[@name='profiles']/pref[@name='default']/pref[@name='command']/@value" -v "/home/$user/.purple/notification.sh" prefs.xml
+echo ""
+
+if [ "$set_notif" == "y" ]
+then
+  user=`whoami`
+  xmlstarlet edit -L -u "/pref[@version='1']/pref[@name='finch']/pref[@name='sound']/pref[@name='profiles']/pref[@name='default']/pref[@name='command']/@value" -v "/home/$user/.purple/notification.sh" prefs.xml
+fi
+

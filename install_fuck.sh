22bf3df80a0a14b9af5109b1dc28871acac3b21c
Sraleik <nicolas.rotier@gmail.com>
2017-02-21 16:13:57 +0100

    ajout du choix de config de l'alias sur fish

--- install_fuck.sh
+++ install_fuck.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 sudo apt update
-sudo apt install python3-dev python3-pip
+sudo apt install python3-dev python3-pip python3-setuptools build-essential
 sudo -H pip3 install thefuck
 
-echo "eval (thefuck --alias fuck | tr '\n' ';')" >> ~/.config/fish/config.fish
+install
+
+###### The Fuck 
+
+echo "-------------"
+echo "Config alias on fish?(y/n)"
+read alias_fish
+
+echo ""
+
+if [ "$alias_fish" == "y" ]
+then
+  echo "eval (thefuck --alias fuck | tr '\n' ';')" >> ~/.config/fish/config.fish
+fi
+
